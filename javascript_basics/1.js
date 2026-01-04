ea4061ea2cc67b986eb63e695bfed03043236044
Preksha Jain <prekshapihu2003@gmail.com>
2026-01-04 15:06:35 +0000

    commit config done

---
+++ javascript_basics/1.js
@@ -0,0 +1,7 @@
+const accountId=1234
+let accountEmail="preksha@google.com"
+var accountPassword="124"
+accountCity="jaipur"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
