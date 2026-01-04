ed0d73d0941decb9e2f61941ce5016c99d1a2241
Preksha Jain <prekshapihu2003@gmail.com>
2026-01-04 16:42:59 +0000

    datatypes conversion

---
+++ javascript_basics/2_typeconversion.js
@@ -0,0 +1,17 @@
+/*let score="44abc"
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber)
+console.log(typeof score)
+console.log(valueinnumber)
+
+let isLoggedIn=1
+let logged="preksha" 
+let logged2=Boolean(logged)
+let booleanLoggedIn=Boolean(isLoggedIn)
+console.log(booleanLoggedIn)    //true
+console.log(logged2)            //true */
+
+let num=44
+let numtostring=String(num)
+console.log(numtostring)
+console.log(typeof numtostring)
