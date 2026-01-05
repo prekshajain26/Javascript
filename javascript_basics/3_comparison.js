122064e5800d33d0896b0d8105f53b70d7b1a6bf
Preksha Jain <prekshapihu2003@gmail.com>
2026-01-05 11:43:00 +0000

    comparison

---
+++ javascript_basics/3_comparison.js
@@ -0,0 +1,12 @@
+// console.log("4">1)
+
+// console.log(null>0)   //false      comparison op
+// console.log(null==0)  //false      equality check operator
+// console.log(null>=0)  //true       comparison op
+
+/*the reason is that comparison convert null to number treating it as 0 
+thats why null>==0 is true and null>0 = false */
+
+//console.log(2>true)  
+
+console.log("2"===2) ///  === means strict comparison that also checks if the datatype od both are same or not.
