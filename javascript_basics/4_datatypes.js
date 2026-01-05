97a149ed210a7b5850c1e187ff126f4ee68ab210
Preksha Jain <prekshapihu2003@gmail.com>
2026-01-05 13:22:36 +0000

    datatypes

---
+++ javascript_basics/4_datatypes.js
@@ -0,0 +1,21 @@
+// const id=Symbol("1234")
+
+// const friends=["preksha","kartik","rahul","dheeraj"];
+
+// let myobj={
+//     "name":"preksha",
+//     "age":22,
+// }
+// console.log(typeof friends)
+// console.log(typeof myobj)
+
+
+// let func1=function(){
+//     console.log("hello preksha")
+
+// }
+// console.log(typeof func1)
+
+/* Primitive datatypes : number,string,symbol,undefined,null,boolean,BigInt
+
+Reference type(non primitive): array([]),object({}),function  */
