a6e2d130e557304e23383c4b940609dad8b1e24a
Preksha Jain <prekshapihu2003@gmail.com>
2026-01-08 12:47:36 +0000

    array

---
+++ javascript_basics/array.js
@@ -0,0 +1,11 @@
+const myArr=[1,4,"preksha",10.4,"true"]
+console.log(myArr);
+
+const newarr=myArr.join()
+console.log(newarr);
+console.log(typeof newarr);
+console.log(myArr.push(7));
+
+
+console.log(myArr.pop())
+console.log(myArr);
