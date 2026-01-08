5b1fae274aba160f9cacefc926f84b97ce09a69d
Preksha Jain <prekshapihu2003@gmail.com>
2026-01-08 15:22:31 +0000

    array

--- javascript_basics/array.js
+++ javascript_basics/array.js
@@ -1,5 +1,5 @@
 const myArr=[1,4,"preksha",10.4,"true"]
-console.log(myArr);
+//console.log(myArr);
 
 const newarr=myArr.join()
 // console.log(newarr);
@@ -9,4 +9,18 @@ const newarr=myArr.join()
 
 // console.log(myArr.pop())
 // console.log(myArr);
-console.log(myArr.includes(4));
+//console.log(myArr.includes(4));
+// myArr.unshift(9)
+
+// console.log(myArr.shift())
+// console.log(myArr);
+// console.log(myArr.shift());
+// console.log(myArr);
+
+console.log("A",myArr);
+const myn1=myArr.slice(1,3)      //slice does'nt gives the last item from the range given and gives the entire array elements 
+console.log(myn1)
+console.log("B",myArr)
+const myn2=myArr.splice(1,3)      //splice gives third index also and removes the splice items from the array and gives the left elements also.
+console.log(myn2)
+console.log("C",myArr);
