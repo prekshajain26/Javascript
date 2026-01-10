d39b32ce5fdac1064c73066d1276071e40281026
Preksha Jain <prekshapihu2003@gmail.com>
2026-01-10 08:25:40 +0000

    objects

---
+++ javascript_basics/objects.js
@@ -0,0 +1,35 @@
+// making objects through object literals
+
+
+// const mySym=Symbol("key1")
+// const jsUser={                               //this is object literal
+//     name:"preksha",
+//     age:21,                                   
+//     email:"preksha@google.com",
+//     location:"jaipur",
+//     lastloggedin:["monday","saturday"],
+//     [mySym]:"mykey1"
+// }
+//  console.log(jsUser.name)
+//  console.log(jsUser["name"])
+//  console.log(jsUser["email"])
+// jsUser.email="preksha26@gmail.com"
+//console.log(jsUser.email);
+//Object.freeze(jsUser)     //freeze the user so that no changes can be made
+//jsUser.email="preksha26@microsoft.com"
+//console.log(jsUser.email)
+
+//console.log("the JS user is ,${this.name}")
+// jsUser.greetings=function(){
+//     console.log("hello JS user");
+// }
+//console.log(jsUser.greetings())
+
+// jsUser.greetings2=function(){
+//     console.log(`the JS user is ${this.name} `);          
+
+// }
+// console.log(jsUser.greetings2());
+
+
+//*************   singleton objects *****************
