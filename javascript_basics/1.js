b0ced5965344e0bc6ba7b8e10a9bb37324660d8a
Preksha Jain <prekshapihu2003@gmail.com>
2026-01-11 06:09:30 +0000

    javas

--- javascript_basics/1.js
+++ javascript_basics/1.js
@@ -1,7 +1,26 @@
-const accountId=1234
-let accountEmail="preksha@google.com"
-var accountPassword="124"
-accountCity="jaipur"
-let accountState;
+// const accountId=1234
+// let accountEmail="preksha@google.com"
+// var accountPassword="124"
+// accountCity="jaipur"
+// let accountState;
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
+//  let n = 5
+//  for (let i = 1; i <= n; i++){
+//      let str="* "; 
+//      let space = '  ' ;
+//      console.log(space.repeat((n-i))+str.repeat(i));
+   
+//  }
+
+
+// let n=5
+// for(i=n;i>=1;i--){
+//     let str="* ";
+//     let space='  '
+//     console.log(space.repeat(n-i)+str.repeat(i))
+// }
+
