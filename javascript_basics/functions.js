74878bb766b29bee74f7d6ced95933bfe137acfa
Preksha Jain <prekshapihu2003@gmail.com>
2026-02-02 04:50:49 +0000

    functions

--- javascript_basics/functions.js
+++ javascript_basics/functions.js
@@ -1,5 +1,38 @@
-function sayMyName(){
-    console.log("preksha");
-    console.log("jain");   
+//************ Method 1 to print something in a function
+// function sayMyName(){
+//     console.log("preksha");
+//     console.log("jain");   
+// }
+// sayMyName()
+
+// ******* Method 2 to print something in a function
+// function loginUser(username){
+//     return `${username} is logged in`
+// }
+//console.log(loginUser("preksha"))
+//console.log(loginUser()); // it will give undefined as we have not passed any value
+
+//so we can also do this
+
+// function loginUser(username){
+//     if (username===undefined){
+//         console.log("please enter a name");
+//         return
+//     }
+
+//     return `${username} is logged in`
+
+// }
+// console.log(loginUser())
+
+//we can also give default value to the parameter so that if dont pass any argument it will automativcally take default value
+
+// function loginUser(username="pihu"){
+//     return `${username} is logged in`
+// }
+// console.log(loginUser());
+
+function shoppingCartPrice(...num){    // to take multiple values when we dont know the number of parametrs
+    return num
 }
-sayMyName()
+console.log(shoppingCartPrice(100,200,300,400));
