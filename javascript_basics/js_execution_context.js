6ee2d6f027677463657183e654ad36eec442843c
Preksha Jain <prekshapihu2003@gmail.com>
2026-02-04 07:58:06 +0000

    execution

---
+++ javascript_basics/js_execution_context.js
@@ -0,0 +1,12 @@
+//      JAVASCRIPT EXECUTION CONTEXT -- means how the js will do the execution of your js files
+
+/* to run the files js divides it into two phases
+now when we give file to js for execution  compulsorily it creates GLOBAL EXECUTION CONTEXT and 
+it is put inside a variable which is called THIS 
+SO,
+1) is GLOBAL EXECUTION CONTEXT
+2) is FUNCTION/FUNCTIONAL EXECUTION CONTEXT
+
+Phases in which the file is executed are -
+1) MEMORY CREATION PHASE (OR CREATION PHASE) - only it allocates space to variables 
+2) 21EXECUTION PHASE -in this phase execution is done
