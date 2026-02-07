53a96e2d1c716c776eeb6deee486483e7e0cd2bd
Preksha Jain <prekshapihu2003@gmail.com>
2026-02-07 13:13:40 +0000

    dsa

--- DSAusingjs/1.js
+++ DSAusingjs/1.js
@@ -1,10 +1,20 @@
-console.log("1"+1) /* it will give 11 because when we giving one string value and adding it with a
+// ****************    TYPE COERCION   ***********************
+// console.log("1"+1)
+ /* it will give 11 because when we giving one string value and adding it with a
 number so our addition sign will convert into concatenation sign and will give 11 because + sign
 has two meanings in coding */
 
-console.log("1"-1) /* it will give 0 only because minus has only one meaning that is to substract 
+// console.log("1"-1)
+ /* it will give 0 only because minus has only one meaning that is to substract 
 so if we are giving one string value it will convert that string into number and then do substraction */
 
-console.log("1"*1) 
-console.log("1"/1)
+// console.log("1"*1) 
+// console.log("1"/1)
 // same concept of minus will be applied to multiplication and division
+ 
+
+//accept and print the number -- prompt()
+//prompt is a function and it returns output as a string 
+// let age=prompt("enter your age");
+// console.log(age)
+// so to convert into number let age=Number(prompt("enter your age")); 
