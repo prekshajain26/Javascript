7316fd4250dcc195ad5a7f270ccccc6fc7b11de7
Preksha Jain <prekshapihu2003@gmail.com>
2026-02-07 12:51:19 +0000

    dsa

---
+++ DSAusingjs/1.js
@@ -0,0 +1,10 @@
+console.log("1"+1) /* it will give 11 because when we giving one string value and adding it with a
+number so our addition sign will convert into concatenation sign and will give 11 because + sign
+has two meanings in coding */
+
+console.log("1"-1) /* it will give 0 only because minus has only one meaning that is to substract 
+so if we are giving one string value it will convert that string into number and then do substraction */
+
+console.log("1"*1) 
+console.log("1"/1)
+// same concept of minus will be applied to multiplication and division
