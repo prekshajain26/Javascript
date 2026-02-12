1a27ab1ae06780cfda2207331f6d9716c13501a4
Preksha Jain <prekshapihu2003@gmail.com>
2026-02-12 16:37:06 +0000

    reduce

--- javascript_basics/filtermapandreduce.js
+++ javascript_basics/filtermapandreduce.js
@@ -10,7 +10,7 @@ const value=myCoding.forEach((item)=>{
 console.log(value) // only this will give values of the array
 */
 
-//***********************        FILTER             *******************
+//***********************                 FILTER             *******************
 // const myNums=[1,2,3,4,5,6,7,8,9,10]
 // const newNums=myNums.filter((num)=>num>4)  here we are not using block scope so return keyworrd is not necessary
 // console.log(newNums)
@@ -32,20 +32,44 @@ myNums.forEach((num)=>{
 })
 console.log(newNums) */
 
-const users=[
-    {name:"preksha",course:"MCA"},
-    {name:"kartik",course:"MCA"},
-    {name:"rahul",course:"MBA"},
-    {name:"dheeraj",course:"MBA"}
+// const users=[
+//     {name:"preksha",course:"MCA"},
+//     {name:"kartik",course:"MCA"},
+//     {name:"rahul",course:"MBA"},
+//     {name:"dheeraj",course:"MBA"}
     
-];
+// ];
 // let userCourse=users.filter((uc)=>uc.course==="MCA")
 // console.log(userCourse)
-userCourse=users.filter((uc)=>{return uc.name==="preksha"})  // as we have used scope so return keyword is necessary
-console.log(userCourse);
+// userCourse=users.filter((uc)=>{return uc.name==="preksha"})  // as we have used scope so return keyword is necessary
+// console.log(userCourse);
 
-// ***************************************************************************************************
- //                                  MAP                    
-const myNumbers=[1,2,3,4,5,6,7,8,9,10]
-const newNums = myNumbers.map((num)=>num+10)
-console.log(newNums);
+// *******************                     MAP                    ********************
+ //                                                       
+ //const myNumbers=[1,2,3,4,5,6,7,8,9,10]
+ //const newNums = myNumbers.map((num)=>num+10)
+// console.log(newNums);
+
+
+// *************          CHAINING--where we can put multiple methods          ****************
+
+/* const newNums=myNumbers
+              .map((num)=>num*10)
+              .map((num)=>num+1 )
+              .filter((num)=>num>40)
+console.log(newNums) */   
+
+// ********************                  REDUCE                 ******************** 
+/*                                 
+const myNums=[1,2,3,4]
+const myTotal=myNums.reduce(function(acc,currval){
+    return acc+currval
+},1) 
+console.log(myTotal)
+
+
+1 is the value of accumulator in first round then it will be added to the 1st value that is 1
+so 1+1=2 now this two will be added to next value that is 2 so 2+2=4
+4+3=7
+7+4=11
+*/
